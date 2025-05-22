1ae6d6069c9c73e50848bdc0bbd3a732381c3130
Dhana-27 <dhanaor27@gmail.com>
2025-05-22 07:55:38 +0530

    Create EchoServer.java

---
+++ EchoServer.java
@@ -0,0 +1,96 @@
+//EchoServer.java 
+
+import java.io.*; 
+
+import java.net.*; 
+
+public class EchoServer{ 
+
+public EchoServer() 
+
+{ 
+
+try 
+
+{ 
+
+} 
+
+server = new ServerSocket(9999); 
+
+catch (Exception err) 
+
+{ 
+
+System.out.println(err); 
+
+} 
+
+} 
+
+public void serve() 
+
+{ 
+
+try 
+
+{ 
+
+while (true) 
+
+{ 
+
+Socket client = server.accept(); 
+
+BufferedReader r = new BufferedReader(new 
+
+InputStreamReader(client.getInputStream())); 
+
+PrintWriter w=new PrintWriter(client.getOutputStream(),true); 
+
+w.println(“Welcome to the Java Echo Server. Type ’bye’ to close.”); 
+
+String line; 
+
+do 
+
+{ 
+
+Line=r.readLine(); 
+
+if (line !=null){w.println(“Got:“+line); 
+
+System.out.println(“received from client:”+line); 
+
+} 
+
+} 
+
+while ( !line.trim().equals("bye") ); 
+
+client.close(); 
+
+} 
+
+} 
+
+catch (Exception err) 
+
+{ 
+
+System.err.println(err); 
+
+} 
+
+}public static void main(String[] args)
+{ 
+
+EchoServer s = new EchoServer(); 
+
+s.serve(); 
+
+} 
+
+private ServerSocket server;
+
+}
